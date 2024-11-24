5f62ebe0eaec9454a982845aff31ef381e79374d
Michael Zhao <44533763+Pistonight@users.noreply.github.com>
2024-11-23 22:03:50 -0800

    update patching code for new libmegaton (#5)
    * update patching code for new libmegaton
    
    * clean up

--- include/toolkit/mem/unique_ptr.hpp
+++ include/toolkit/mem/unique_ptr.hpp
@@ -14,13 +14,13 @@ struct raw_free {
 
 template <typename T> using unique_ptr = std::unique_ptr<T, raw_free>;
 
-template <typename T> unique_ptr<T> make_unique() {
+template <typename T, class... Args > unique_ptr<T> make_unique( Args&&... args ) {
     T* ptr = static_cast<T*>(malloc(sizeof(T)));
     if (!ptr) {
         return {};
     }
     unique_ptr<T> uptr{ptr};
-    new (ptr) T();
+    new (ptr) T(std::forward<Args>(args)...);
     return std::move(uptr);
 }
 
